a6bf5e61833a7b192baa359792f61930f4e278f9
Alexandru Tofan <alextofan16@gmail.com>
2017-03-18 12:25:31 +0200

    first commit

---
+++ app/src/main/java/ro/fmi/bikewithme/bikewithme/Manifest.java
@@ -0,0 +1,10 @@
+package ro.fmi.bikewithme.bikewithme;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * created for manifest activity
+ */
+
+public class Manifest extends AppCompatActivity {
+}
